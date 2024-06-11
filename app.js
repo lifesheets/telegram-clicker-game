2a9ddd35b722c6d72c72eef0fbc0ee4c9026b03d
ua.lifesheets <ua.lifesheets@gmail.com>
2024-06-11 20:08:46 +0300

    Basic code for debugging an application.

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ const helmet = require('helmet');
 const Sequelize = require('sequelize');
 const TelegramBot = require('node-telegram-bot-api');
 const winston = require('./utils/logger');
+const debug = require('./utils/debug')('app:server');
---
+++ utils/debug.js
@@ -0,0 +1,5 @@
+const debug = require('debug');
+
+module.exports = (namespace) => {
+    return debug(namespace);
+};
