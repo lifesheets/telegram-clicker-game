cb0592b1f2456f5eb3f11e057502853c2608335e
ua.lifesheets <ua.lifesheets@gmail.com>
2024-06-11 22:05:57 +0300

    Self-signed SSL certificate for telegram application development.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 require('dotenv').config();
 
@@ -6,5 +6,13 @@ const express = require('express');
 const helmet = require('helmet');
 const Sequelize = require('sequelize');
 const TelegramBot = require('node-telegram-bot-api');
-const { logger, createDebug } = require('./system/methods'); 
+const { logger, createDebug } = require('./system/methods');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const sslOptions = {
+  key: fs.readFileSync('openssl/server.key'),
+  cert: fs.readFileSync('openssl/server.crt')
+};
