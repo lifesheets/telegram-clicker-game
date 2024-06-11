8ca7af3a398d735a6025baca04609a9ce8749d36
ua.lifesheets <ua.lifesheets@gmail.com>
2024-06-11 22:07:46 +0300

    Start command for telegram

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ const sslOptions = {
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+  const telegramId = msg.from.id;
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [{
+          text: 'Грати',
+          web_app: { url: 'https://dev-clicker.local' }
+        }]
+      ]
+    }
+  };
+  try {
+    await User.findOrCreate({ where: { telegramId } });
+    bot.sendMessage(chatId, 'Добро пожаловать в игру кликер! Начните кликать, чтобы зарабатывать монеты.', options);
+  } catch (err) {
+    logger.error('Error on /start command', err);
+    bot.sendMessage(chatId, 'Произошла ошибка при запуске игры.');
+  }
+});
